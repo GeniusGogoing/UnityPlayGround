04c26555f49f986c536d9d724f23aca4fcdca405
Harry <646334288@qq.com>
2022-03-13 10:43:38 +0800

    <feature> Init

---
+++ Assets/Editor/InspectorEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Player))]
+public class InspectorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Player player = (Player)target;
+        player.exp = EditorGUILayout.IntField("Exp", player.exp);
+        EditorGUILayout.LabelField("level", player.Level.ToString());
+        EditorGUILayout.HelpBox("this is a help box", MessageType.None);
+        if (GUILayout.Button("Add 1000 exp !"))
+        {
+            player.AddExp(1000);
+        }
+    }
+}
---
+++ Assets/Scripts/Tool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Tool
+{
+    [MenuItem("Tool/Clear PlayerPrefs %#C")]
+    static void ClearPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+
+    //Assets Assets/Create
+    [MenuItem("CONTEXT/Player/AddExp2000")]
+    static void PlayerAddExp(MenuCommand menuCommand)
+    {
+        Player player = menuCommand.context as Player;
+        player.AddExp(2000);
+    }
+
+    [MenuItem("CONTEXT/Player/AddExp", true)]
+    static bool PlayerAddExpValidation()
+    {
+        return Selection.activeObject.name == "Player";
+    }
+
+    [MenuItem("CONTEXT/Player/ReduceExp2000", false, 1)]
+    static void PlayerReduceExp(MenuCommand menuCommand)
+    {
+        Player player = menuCommand.context as Player;
+        player.AddExp(-2000);
+    }
+}
